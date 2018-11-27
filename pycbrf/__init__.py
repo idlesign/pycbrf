d6933c26e1b09f31302d1932c69fe50c6aa10dfd
idle sign <idlesign@yandex.ru>
2018-11-27 20:50:54 +0700

    Release v0.3.0

--- pycbrf/__init__.py
+++ pycbrf/__init__.py
@@ -2,7 +2,7 @@ from .banks import Banks
 from .rates import ExchangeRates
 
 
-VERSION = (0, 2, 4)
+VERSION = (0, 3, 0)
 """Application version number tuple."""
 
 VERSION_STR = '.'.join(map(str, VERSION))
