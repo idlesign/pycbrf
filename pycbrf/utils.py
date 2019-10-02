5b5495701684e4d5f378c7311d29d6f3fded2101
Igor Starikov <idlesign@yandex.ru>
2019-10-02 09:48:06 +0700

    Added 'WithRequests' helper

--- pycbrf/utils.py
+++ pycbrf/utils.py
@@ -1,5 +1,7 @@
 import sys
 
+import requests
+
 
 PY3 = sys.version_info[0] == 3
 
@@ -12,3 +14,26 @@ else:  # pragma: nocover
     string_types = basestring,
     import StringIO
     BytesIO = StringIO.StringIO
+
+
+class WithRequests(object):
+
+    req_timeout = 10
+    
+    req_user_agent = (
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Chrome/74.0.3729.169 YaBrowser/19.6.2.594 (beta) Yowser/2.5 Safari/537.36'
+    )
+
+    @classmethod
+    def _get_response(cls, url, **kwargs):
+
+        kwargs_ = {
+            'timeout': cls.req_timeout,
+            'headers': {
+                'User-Agent': cls.req_user_agent
+            },
+        }
+        kwargs_.update(kwargs)
+
+        return requests.get(url, **kwargs_)
