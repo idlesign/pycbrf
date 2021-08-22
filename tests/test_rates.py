c56c5cf5a0d81858e1f08f24cef206969f1dbb9e
Eugene Suhanov <suhanoves@gmail.com>
2021-08-22 19:52:59 +0300

    Add tests for Currency and CurrenciesLib

--- tests/test_rates.py
+++ tests/test_rates.py
@@ -1,8 +1,9 @@
-from pycbrf import ExchangeRates
+from decimal import Decimal
+
+from pycbrf import ExchangeRates, CurrenciesLib, Currency
 
 
 def test_rates():
-
     rates = ExchangeRates('2016-06-26', locale_en=True)
 
     assert str(rates.date_requested) == '2016-06-26 00:00:00'
@@ -23,3 +24,30 @@ def test_rates():
     assert rates['USD'].name == 'Доллар США'
     assert rates['R01235'].name == 'Доллар США'
     assert rates['840'].name == 'Доллар США'
+
+
+def test_currencieslib():
+    lib = CurrenciesLib()
+    aud = Currency(
+        id='R01010',
+        name_eng='Australian Dollar',
+        name_ru='Австралийский доллар',
+        num='036',
+        code='AUD',
+        par=Decimal(1)
+    )
+
+    assert lib['dummy'] is None
+    assert lib['None'] is None
+    assert lib[None] is None
+
+    assert lib['aud'] == aud
+    assert lib['AUD'] == aud
+    assert lib['R01010'] == aud
+    assert lib['r01010'] == aud
+    assert lib['036'] == aud
+    assert lib['36'] == aud
+    assert lib[36] == aud
+
+    assert lib['usd'].name_ru == 'Доллар США'
+    assert lib['USD'].name_eng == 'US Dollar'
