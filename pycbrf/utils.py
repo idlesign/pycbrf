29a801a8502d84284d6a37e3bf5decc104e87d89
Eugene Suhanov <suhanoves@gmail.com>
2021-08-23 23:18:01 +0300

    Some of the identical methods have been moved to the mixin

--- pycbrf/utils.py
+++ pycbrf/utils.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from typing import Union
+
 import requests
 
 
@@ -5,7 +8,7 @@ class WithRequests:
     """Mixin to perform HTTP requests."""
 
     req_timeout: int = 10
-    
+
     req_user_agent: str = (
         'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
         'Chrome/74.0.3729.169 YaBrowser/19.6.2.594 (beta) Yowser/2.5 Safari/537.36'
@@ -13,7 +16,6 @@ class WithRequests:
 
     @classmethod
     def _get_response(cls, url: str, **kwargs) -> requests.Response:
-
         kwargs_ = {
             'timeout': cls.req_timeout,
             'headers': {
@@ -33,3 +35,21 @@ class SingletonMeta(type):
             instance = super().__call__()
             cls._instances[cls] = instance
         return cls._instances[cls]
+
+
+class FormatMixin:
+    @staticmethod
+    def _format_num_code(num: Union[int, str]) -> str:
+        """Format integer or invalid string numeric code to ISO 4217 currency numeric code."""
+        if isinstance(num, int) or (isinstance(num, str) and len(num) < 3):
+            num_ = num
+            if isinstance(num_, str):
+                num_ = int(num_)
+            return "{:03}".format(num_)
+        return num
+
+    @staticmethod
+    def _date_from_string(date):
+        if isinstance(date, str):
+            return datetime.strptime(date, '%Y-%m-%d')
+        return date
