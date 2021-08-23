30c45b1c291176bc5b551601498ecfe40e8f008e
Eugene Suhanov <suhanoves@gmail.com>
2021-08-23 23:19:28 +0300

    Add custom Exceptions

--- pycbrf/exceptions.py
+++ pycbrf/exceptions.py
@@ -1,3 +1,23 @@
-
 class PycbrfException(Exception):
     """Base exception."""
+
+
+class WrongArguments(PycbrfException, TypeError):
+    """The exception is raised if the passed arguments are insufficient or invalid"""
+
+    def __init__(self, message=None):
+        self.message = message
+
+
+class CurrencyNotExists(PycbrfException, KeyError):
+    """The exception is raised if Currency does not found in CurrenciesLib"""
+
+    def __init__(self, message=None):
+        self.message = message or 'There is no such currency within CurrenciesLib'
+
+
+class ExchangeRateNotExists(PycbrfException, KeyError):
+    """The exception is raised if ExchangeRate does not found in ExchangeRates"""
+
+    def __init__(self, message=None):
+        self.message = message or 'There is no such ExchangeRate within ExchangeRates'
