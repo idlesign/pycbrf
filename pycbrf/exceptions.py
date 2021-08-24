1038efb99c1d8c63a8c086357607f86fce4e9681
Eugene Suhanov <suhanoves@gmail.com>
2021-08-24 13:35:26 +0300

    Add BetaExchangeRate and BetaExchangeRates classes, refactoring

--- pycbrf/exceptions.py
+++ pycbrf/exceptions.py
@@ -13,11 +13,11 @@ class CurrencyNotExists(PycbrfException, KeyError):
     """The exception is raised if Currency does not found in CurrenciesLib"""
 
     def __init__(self, message=None):
-        self.message = message or 'There is no such currency within CurrenciesLib'
+        self.message = message or 'There is no such currency within CurrenciesLib.'
 
 
 class ExchangeRateNotExists(PycbrfException, KeyError):
     """The exception is raised if ExchangeRate does not found in ExchangeRates"""
 
     def __init__(self, message=None):
-        self.message = message or 'There is no such ExchangeRate within ExchangeRates'
+        self.message = message or 'There is no such ExchangeRate within ExchangeRates.'
--- pycbrf/rates.py
+++ pycbrf/rates.py
@@ -1,7 +1,7 @@
-from datetime import datetime
+from datetime import datetime, date
 from decimal import Decimal
 from logging import getLogger
-from typing import Dict, List, NamedTuple, Optional, Tuple, Union
+from typing import Dict, List, NamedTuple, Optional, Tuple, Union, Type
 from xml.etree import ElementTree
 
 from .exceptions import WrongArguments, CurrencyNotExists, ExchangeRateNotExists
@@ -52,7 +52,7 @@ class CurrenciesLib(WithRequests, FormatMixin, metaclass=SingletonMeta):
     Attributes:
         length (int):
             the number of different currencies in the library
-        update_date (datetime):
+        update_date (date):
             date of loading the latest information from www.cbr.ru
         currencies (dict of Currency):
             a {Union[Currency.id, Currency.num, Currency.code]: Currency} dictionary for all currencies
@@ -138,12 +138,12 @@ class CurrenciesLib(WithRequests, FormatMixin, metaclass=SingletonMeta):
         LOG.debug(f"Parsed: {self.length} currencies")
         return currencies
 
-    def __getitem__(self, item: Union[int, str]) -> Optional[Currency]:
+    def __getitem__(self, value: Union[int, str]) -> Optional[Currency]:
         """Returns Currency by dictionary lookup, converting the argument to ISO format."""
-        if not item:
+        if not value:
             raise WrongArguments(f"You must pass ISO code, numeric code or code the Bank of Russia of currency or "
-                                 f"Currency instance. Not {'None' if item is None else 'empty string'}.")
-        item_ = self._format_num_code(item)
+                                 f"Currency instance. Not {'None' if value is None else 'empty string'}.")
+        item_ = self._format_num_code(value)
         item_ = item_.lower()
 
         try:
@@ -154,10 +154,296 @@ class CurrenciesLib(WithRequests, FormatMixin, metaclass=SingletonMeta):
         return currency
 
     def __str__(self):
-        return f"CurrenciesLib of {self.length} currencies. Update {datetime.strftime(self.update_date, '%Y-%m-%d')}"
+        return f"CurrenciesLib of {self.length} currencies. Update {date.strftime(self.update_date, '%Y-%m-%d')}"
 
     def __len__(self):
         return self.length
+
+
+class BetaExchangeRate(NamedTuple):
+    """Rethinking ExchangeRate class
+    Represents exchange rate for the currency on the date
+
+    Attributes: # noqa
+        curency (Currency):
+            Currency for which the rate is fetched.
+        date (datetime.date):
+            Date of the exchange rate.
+        nominal (Decimal):
+            Nominal exchange rate
+        value (Decimal):
+            Official exchange rates on selected date against the ruble
+        rate (Decimal):
+            Reduced exchange rate
+        dates_match (bool):
+            Returns whether the requested rate date and response rate date are the same.
+            Because the Central Bank of Russia does not change rates on weekends and holidays.
+            The Sunday and Monday rates is set as equal as Saturday rate.
+            The rates of holiday is set as equal on the last working day before the holidays.
+    """
+    currency: Currency
+    date: datetime.date
+    nominal: Decimal
+    value: Decimal
+    rate: Decimal
+    date_requested: date
+    date_received: date
+    dates_match: Optional[bool]
+
+    @property
+    def id(self):
+        return self.currency.id
+
+    @property
+    def name_ru(self):
+        return self.currency.name_ru
+
+    @property
+    def name_eng(self):
+        return self.currency.name_eng
+
+    @property
+    def num(self):
+        return self.currency.num
+
+    @property
+    def code(self):
+        return self.currency.code
+
+
+class BetaExchangeRates(WithRequests, FormatMixin):
+    """Rethinking ExchangeRates class
+    Fetches and collect exchange rates.
+
+    Attributes:
+        date_from(str, datetime.date):
+            Start date of the period for the exchange rate dynamics
+        date_to(str, datetime.date):
+            End date of the period for the exchange rate dynamics.
+            Python date objects and ISO date '%Y-%m-%d' string are supported.
+            If rates is requested in one day is equal to date_from
+        is_multicurrency (bool):
+            True if the collection contains multicurrency rates for a specific date.
+            False if the collection contains rate dynamics for one currency.
+        currencies_lib (CurrenciesLib):
+            Library of currencies.
+            Not passed, create during class initialization
+    """
+
+    def __init__(self,
+                 date_from: Union[date, str, None] = None,
+                 date_to: Union[date, str, None] = None,
+                 *,
+                 currency: Union[str, int, Currency] = None,
+                 ):
+        """
+        Args:
+            date_from:
+                Date of the exchange rate or start date of the period for the exchange rate dynamics.
+                Python date objects and ISO date '%Y-%m-%d' string are supported
+            date_to:
+                End date of the period for the exchange rate dynamics
+                Python date objects and ISO date '%Y-%m-%d' string are supported
+            currency:
+                Currency for which you need to know the rate.
+                Strings like ISO numeric code, ISO code or code the Bank of Russia or Currency instance are supported.
+
+        """
+        self.currencies_lib = CurrenciesLib()
+        self.date_from, self.date_to, currency_ = self._check_and_convert_args(date_from, date_to, currency)
+
+        if currency:
+            self.is_multicurrency = False
+            raw_data = self._get_rate_dynamics_data(currency_)
+            self.rates = self._parse_rate_dynamics(raw_data)
+        else:
+            self.is_multicurrency = True
+            raw_data = self._get_multicurrency_rates_data()
+            self.rates = self._parse_multicurrency_rates(raw_data)
+
+    def _check_and_convert_args(self, date_from, date_to, currency) -> Tuple[date, date, Currency]:
+        """Checks arguments and converts them to proper formats from strings and integer"""
+
+        date_from = self._date_from_string(date_from)
+        date_to = self._date_from_string(date_to)
+
+        if date_to and date_from and date_from != date_to and not currency:
+            raise WrongArguments('You must specify a currency if you want to get a range of currency rates.')
+
+        if not date_from and not date_to:  # if both dates are empty
+            date_from = date_to = date.today()
+        elif bool(date_from) ^ bool(date_to):  # if any of the dates, but not both (XOR)
+            date_from = date_to = next(filter(lambda x: x, (date_from, date_to)))
+
+        if date_to < date_from:
+            raise WrongArguments('The end date of the period must be later than the start date.')
+
+        if currency and not isinstance(currency, Currency):
+            currency: Currency = self.currencies_lib[currency]
+
+        return date_from, date_to, currency
+
+    def _get_rate_dynamics_data(self, currency: Currency) -> bytes:
+        """If the currency is passed, get a rate dynamics XML string for a specific currency.
+
+        If all arguments are specified, get the course dynamics between the passed dates.
+        If currency and one date are passed, get the rate of the specified currency at the date.
+        If only currency is passed, get the exchange rate for today."""
+
+        url_suffix = 'XML_dynamic'
+        params = {
+            'date_req1': self.date_from.strftime('%d/%m/%Y'),
+            'date_req2': self.date_to.strftime('%d/%m/%Y'),
+            'VAL_NM_RQ': currency.id,
+        }
+
+        raw_data = self._get_data(suffix=url_suffix, params=params)
+
+        return raw_data
+
+    def _get_multicurrency_rates_data(self) -> bytes:
+        """If no currency is passed, get a multicurrency rates XML string for a specific date.
+
+        If date_from or date_to are passed, get multicurrency rates on the specified date.
+        If no date is passed, get the multicurrencies courcies for today.
+        If date_from and date_to are passed, initiate checks.
+        """
+
+        url_suffix = 'XML_daily'
+        params = {
+            'date_req': self.date_from.strftime('%d/%m/%Y'),
+        }
+
+        raw_data = self._get_data(suffix=url_suffix, params=params)
+
+        return raw_data
+
+    @classmethod
+    def _get_data(cls, suffix, params) -> bytes:
+        """Returns XML string from link"""
+        url = f"{URL_BASE}{suffix}.asp"
+
+        LOG.debug(f'Getting exchange rates from {url} ...')
+
+        response = cls._get_response(url, params=params)
+        data = response.content
+
+        return data
+
+    def _parse_rate_dynamics(self, data) -> Dict[date, BetaExchangeRate]:
+        """Parse raw XML strings with rate dynamics to the dict of BetaExchangeRates"""
+        LOG.debug('Parsing rate dynamics data ...')
+
+        xml = ElementTree.fromstring(data)
+        currency = self.currencies_lib[xml.attrib['ID']]
+
+        result = {}
+
+        for child in xml:
+            elems = {}
+            for elem in child:
+                elems[elem.tag] = elem.text
+
+            date_received = datetime.strptime(child.attrib['Date'], '%d.%m.%Y').date()
+            nominal = Decimal(elems['Nominal'])
+            value = Decimal(elems['Value'].replace(',', '.'))
+
+            result[date_received] = BetaExchangeRate(
+                currency=currency,
+                date=date_received,
+                nominal=nominal,
+                value=value,
+                rate=value / nominal,
+                date_requested=date_received,
+                date_received=date_received,
+                dates_match=True,
+            )
+
+        LOG.debug(f"Parsed: {len(result)} days")
+
+        return result
+
+    def _parse_multicurrency_rates(self, data) -> Dict[Currency, BetaExchangeRate]:
+        """Parse raw XML strings with multicurrency reates to the dict of BetaExchangeRates"""
+        LOG.debug('Parsing multicurrency reates data ...')
+
+        xml = ElementTree.fromstring(data)
+
+        date_requested = self.date_from
+        date_received = datetime.strptime(xml.attrib['Date'], '%d.%m.%Y').date()
+        dates_match = date_requested == date_received
+
+        result = {}
+
+        for child in xml:
+            elems = {}
+            for elem in child:
+                elems[elem.tag] = elem.text
+
+            try:
+                currency = self.currencies_lib[child.attrib['ID']]
+            except CurrencyNotExists:
+                # If you are requesting really old information, the rates may contain a currency,
+                # that has already been removed from the CurrenciesLib.
+                # In this case, add the currency to the CurrenciesLib.
+                currency = Currency(
+                    id=child.attrib['ID'],
+                    name_eng=elems['Name'],
+                    name_ru=elems['Name'],
+                    code=elems['CharCode'],
+                    num=elems['NumCode'],
+                    nominal=Decimal(elems['Nominal']),
+                )
+                self.currencies_lib.add(currency)
+
+            nominal = Decimal(elems['Nominal'])
+            value = Decimal(elems['Value'].replace(',', '.'))
+
+            result[currency] = BetaExchangeRate(
+                currency=currency,
+                date=date_received,
+                nominal=nominal,
+                value=value,
+                rate=value / nominal,
+                date_requested=date_requested,
+                date_received=date_received,
+                dates_match=dates_match,
+            )
+
+            LOG.debug(f"Parsed: {len(result)} currencies")
+
+        return result
+
+    def __getitem__(self, value: Union[Currency, date, str, int]) -> BetaExchangeRate:
+        """Returns the ExchangeRate by date or by currency depending on the ExchangeRates type.
+        If ExchangeRates is multicurrency find by id, num and code params of Currency, or Currency class.
+        If ExchangeRates is multidate find by date objects or '%Y-%m-%d' ISO date string
+        """
+        if not value:
+            raise WrongArguments(f"Args must be ISO code, numeric code, code the Bank of Russia of currency, "
+                                 f"Currency instance, datetime.date or  or '%Y-%m-%d' ISO date string."
+                                 f"Not {'None' if value is None else 'empty string'}.")
+
+        if self.is_multicurrency:
+            key_str = self._format_num_code(value)
+            key = self.currencies_lib[key_str]
+        else:
+            if isinstance(value, datetime):
+                key = value.date()
+            else:
+                key = self._date_from_string(value)
+
+        try:
+            return self.rates[key]
+        except KeyError:
+            raise ExchangeRateNotExists()
+
+    def __len__(self):
+        return len(self.rates)
+
+    def __str__(self):
+        return f"{'MulticurrencyExchangeRates' if self.is_multicurrency else 'MultidatesExchangeRates'} " \
+               f"of {len(self.rates)} rates"
 
 
 class ExchangeRate(NamedTuple):
